1eb28bcbaba0832cecc066e33ed80cbc8aae29d4
Maximilian Krüger <Maximilian.Krueger@airsphere.de>
2019-02-20 10:03:20 +0100

    Add csharp wrapper as submodule
    * Add csharp wrapper from librealsense.csharp
    * Use submodule logic for project
    * Remove old and redundand code

--- wrappers/csharp/Intel.RealSense/Types/RecordingMode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Intel.RealSense.Types
-{
-    public enum RecordingMode
-    {
-        BlankFrames = 0,
-        Compressed = 1,
-        BestQuality = 2
-    }
-}
--- wrappers/csharp/Intel.RealSense/Types/TimestampDomain.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Intel.RealSense.Types
-{
-    public enum TimestampDomain
-    {
-        HardwareClock = 0,
-        SystemTime = 1
-    }
-}
